daf3c8632986c7cd65966058d360d3480a5b691a
jelsbrock <jack.elsbrock@gmail.com>
2022-12-20 22:16:00 -0600

    updates

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,6 +2,7 @@ function init(){
   dropDown()
   barChart('ALABAMA')
   lineChart('ALABAMA')
+  map()
 }
 init()
 
@@ -88,55 +89,35 @@ function barChart(stateNames){
 
   };
 
+function map(){
   var myMap = L.map("map", {
       center: [37.09, -95.71],
       zoom: 5
     });
-  
-  
 
-    // // Add a tile layer.
-    // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    // }).addTo(myMap);
-    
-    // // State markers
-    // var states = [{
-    //   location: [32.51, -86.5],
-    //   name: "Alabama",
-    //   population: "x"
-    // },
-    // {
-    //   location: [64.44, -152.28],
-    //   name: "Alaska",
-    //   population: "x"
-    // },
-    // {
-    //   location: [34.19, -111.48],
-    //   name: "Arizona",
-    //   population: "x"
-    // },
-    // {
-    //   location: [34.49, -92.18],
-    //   name: "Arkansas",
-    //   population: "x"
-    // },
-    // {
-    //   location: [36.6, -120.5],
-    //   name: "California",
-    //   population: "x"
-    // },
-    // {
-    //   location: [38.6, -105.39],
-    //   name: "Colorado",
-    //   population: "x"
-    // }
-    // ];
-    
-    // // Add code to create a marker for each of the following cities and to add it to the map.
-    // for (var i = 0; i < states.length; i++) {
-    //   var state = states[i]
-    //   L.marker(state.location)
-    //   .bindPopup(`<h1>${state.name}</h1> <hr> <h3>Population ${state.population.toLocaleString()}</h3>`)
-    //   .addTo(myMap);
-    // };
+    var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(myMap);
+
+    L.geoJson(statesData).addTo(myMap);
+
+  // function onEachFeature(feature, layer) {
+  //   layer.on({
+  //     click: layer.bindPopup(`<h1>${statesData.properties.name}</h1>
+  //     <hr>
+  //     <h3>Colonies: ${statesData.properties.bee_pop}</h3>`)
+  //   });
+  // }
+
+  var marker = L.marker([51.5, -0.09]).addTo(myMap);
+    // Bind a mouseover event to the marker
+    marker.on('mouseover', function (e) {
+    // Open a popup with information about the marker
+    var popup = L.popup()
+        .setLatLng(e.properties.geometry.coordinates)
+        .setContent(e.properties.name)
+        .openOn(myMap);
+  });
+};
+map()
